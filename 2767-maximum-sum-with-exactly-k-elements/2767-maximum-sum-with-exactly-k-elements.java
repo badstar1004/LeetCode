1c69805c50b8eb6804bf3e599515dc3e6a971fdc
김은정 <32833969+badstar1004@users.noreply.github.com>
2023-08-08 22:29:03 +0900

    Time: 6 ms (52.93%), Space: 43.8 MB (70.56%) - LeetHub

---
+++ 2767-maximum-sum-with-exactly-k-elements/2767-maximum-sum-with-exactly-k-elements.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximizeSum(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int lastNum = nums[nums.length - 1];
+        int sum = 0;
+        for(int i = 0; i < k; i++) {
+            sum += (lastNum + i);
+        }
+
+        return sum;
+    }
+}
