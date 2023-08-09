7b3a349d8bfc22f8e1615f4f2b7ef0342ffaad33
김은정 <32833969+badstar1004@users.noreply.github.com>
2023-08-09 22:57:49 +0900

    Time: 5 ms (62.42%), Space: 56.2 MB (93.19%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        // 플로이드의 토끼와 거북이 (Floyd's Tortoise and Hare) 알고리즘
+        // 순환구조의 존재 여부
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // 순환 구조의 시작점
+        slow = nums[0];
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+}
