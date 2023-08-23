ab4b421d1fea58a34adf255a941d8bdf466d81e5
김은정 <32833969+badstar1004@users.noreply.github.com>
2023-08-23 22:05:06 +0900

    Time: 0 ms (100%), Space: 43.9 MB (46.33%) - LeetHub

---
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] answer = new int[n * 2];
+
+        for(int i = 0; i < n; i++) {
+            answer[2 * i] = nums[i];
+            answer[2 * i + 1] = nums[i + n];
+        }
+
+        return answer;
+    }
+}
