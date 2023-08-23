441b7626da628ca6dcd8119d5ffbf728f86444cd
김은정 <32833969+badstar1004@users.noreply.github.com>
2023-08-23 22:07:06 +0900

    Time: 1 ms (10.35%), Space: 41.7 MB (99.59%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.java
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -5,8 +5,8 @@ class Solution {
         for(int i = 0; i < n; i++) {
             answer[2 * i] = nums[i];
             answer[2 * i + 1] = nums[i + n];
+            
         }
-
         System.gc();
         return answer;
     }
