db6c0b8397d27ed9f8234b2e00bb3ba0cbe65d63
김은정 <32833969+badstar1004@users.noreply.github.com>
2023-08-28 22:54:57 +0900

    Time: 1 ms (42.64%), Space: 40.8 MB (88.7%) - LeetHub

---
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int count = 0;
+
+        for(int n : hours) {
+            if(n >= target) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
