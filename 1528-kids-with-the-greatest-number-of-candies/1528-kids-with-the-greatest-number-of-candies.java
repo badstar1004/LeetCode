146a1699eb336cf6653755d75df6e483dc7accd0
김은정 <32833969+badstar1004@users.noreply.github.com>
2023-08-29 20:35:30 +0900

    Time: 1 ms (84.03%), Space: 41.2 MB (90.86%) - LeetHub

---
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> answer = new ArrayList<>();
+
+        int max = 0;
+        for(int candy : candies) {
+            max = Math.max(max, candy);
+        }
+
+        for (int candy : candies) {
+            answer.add(candy + extraCandies >= max);
+        }
+
+        return answer;
+    }
+}
