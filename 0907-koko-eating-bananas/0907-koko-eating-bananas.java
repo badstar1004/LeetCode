0721fdcfaa68d2af3ce771cda395cecf28a5becc
김은정 <32833969+badstar1004@users.noreply.github.com>
2023-08-29 21:31:52 +0900

    Time: 33 ms (31.86%), Space: 44 MB (77.42%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        Arrays.sort(piles);
+        int len = piles.length;
+        int start = 1;
+        int end = piles[len-1];
+
+        while(start < end){
+            int speed = (start+end) / 2;  // 중간 스피드부터 시작
+            int time = 0;
+            for (int i = 0; i < len; i++) {
+                time += Math.ceil((double)piles[i]/speed);
+                if(time > h){ //다 못먹었는데 시간 초과됨, 속도를 올려야함
+                    start = speed+1;
+                    break;
+                }
+            }
+            if(time <= h){  //시간안에 다 먹었으면 속도를 줄여서 다시 확인
+                end = speed;
+            }
+        }
+        return start;
+    }
+}
